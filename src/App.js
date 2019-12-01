7233fdda2518e5a8abb0cf57e28b144a3803d112
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-12-01 13:40:54 -0800

    Remove router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router} from 'react-router-dom';
 import Home from './Home.js';
 import {store} from './Guides.js';
 import {Provider} from 'react-redux';
@@ -9,9 +8,7 @@ import {Provider} from 'react-redux';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route component={Home} exact={true} path='/'/>
-      </Router>
+      <Home/>
     );
   }
 }
